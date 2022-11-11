8dea11373d3e51433f42106c9a519d49616eed8e
NadezhdaFilippova <n9685232364@yandex.ru>
2022-11-11 17:49:59 +0300

    corrects scroll

--- source/js/modules/scroll-to-form.js
+++ source/js/modules/scroll-to-form.js
@@ -1,20 +1,33 @@
-const consultForm = document.getElementById('scroll');
+// const consultForm = document.getElementById('scroll');
 const scrollButton = document.getElementById('scroll');
 
 const initScrollToForm = () => {
-
-
-  const scrollButtonClick = () => {
-    const target = consultForm;
-    const elementPosistion = target.getBoundingClientRect().top;
-    const offSetPosition = elementPosistion;
-
-    window.scrollBy({
-      top: offSetPosition,
-      behavior: 'smooth',
+  if (scrollButton) {
+    scrollButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      const blockID = scrollButton.getAttribute('href');
+      blockID.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
     });
-  };
-  scrollButton.addEventListener('click', scrollButtonClick);
+
+  }
 };
+// const initScrollToForm = () => {
+//   const scrollTo = ()=>{
+//     const step = 2;
+//     if (consultForm.offsetTop < window.pageYOffset + step) {
+//       document.documentElement.scrollTo(0, consultForm.offsetTop);
+//     } else {
+//       document.documentElement.scrollTo(0, window.pageYOffset + step);
+//     }
+//     if (consultForm.offsetTop !== window.pageYOffset) {
+//       requestAnimationFrame(scrollTo);
+//     }
+//   };
+
+//   scrollButton.addEventListener('click', scrollTo);
+// };
 
 export {initScrollToForm};
